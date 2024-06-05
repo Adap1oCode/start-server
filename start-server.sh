ca6a9b83948a1c54535a43b3d436e4cc2dab32f2
Adap1oCode <155316255+Adap1oCode@users.noreply.github.com>
2024-06-05 18:21:13 +0100

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+update_system() {
+    sudo apt update
+    sudo apt install -y git nano curl wget
+}
 
 # Function to change the hostname
 change_hostname() {
@@ -19,27 +23,41 @@ change_hostname() {
 
 # Function to run specified commands
 run_commands() {
-    sudo apt update
-    sudo apt install -y git nano curl wget
-
     # Remove and recreate the .ssh directory
     rm -r ~/.ssh
     mkdir ~/.ssh
 
-    # Generate a new SSH key
+    # Generate a new SSH key with .adaplo.co.uk appended to the hostname
     hostname=$(hostname)
-    ssh-keygen -t rsa -b 4096 -C "$hostname" -f ~/.ssh/$hostname
+    full_hostname="$hostname.adaplo.co.uk"
+    ssh-keygen -t rsa -b 4096 -C "$full_hostname" -f ~/.ssh/$full_hostname
     eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/$hostname
+    ssh-add ~/.ssh/$full_hostname
 
     # Display the public key
-    cat ~/.ssh/$hostname.pub
+    cat ~/.ssh/$full_hostname.pub
 
     # Prompt user to type 'c' to continue
     read -p "Type 'c' to continue after copying the public key: " confirm
     while [ "$confirm" != "c" ]; do
         read -p "Invalid input. Please type 'c' to continue: " confirm
     done
+}
+
+update_dns_servers() {
+    # Backup the current resolv.conf
+    sudo cp /etc/resolv.conf /etc/resolv.conf.bak
+
+    # Clear current DNS server entries
+    sudo sed -i '/^nameserver/d' /etc/resolv.conf
+
+    # Add Cloudflare and Google DNS servers
+    echo "nameserver 1.1.1.1" | sudo tee -a /etc/resolv.conf
+    echo "nameserver 1.0.0.1" | sudo tee -a /etc/resolv.conf
+    echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+    echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
+
+    echo "DNS servers updated successfully."
 }
 
 # Function to run the GitHub setup commands
@@ -52,9 +70,12 @@ run_github_setup() {
 
 # Main function to prompt user
 main() {
+    update_system
+    update_dns_servers
     current_hostname=$(hostname)
     echo "The current hostname is: $current_hostname"
     change_hostname
+    update_dns_servers
     run_commands
     run_github_setup
      
